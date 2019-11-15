f7c25d1922cdc564201b13d80be4d1d63665c5dc
Nikolai Gladchenko <ngl@kuk-is.de>
2019-11-15 20:37:55 +0100

    nuget packages added

---
+++ app/dotnet.js
@@ -0,0 +1,45 @@
+const { execSync, exec } = require('child_process');
+const chalk = require('chalk');
+
+function formated_exec_sync(command) {
+    console.log("exec->" + command);
+    execSync(command, {stdio: 'inherit'});
+}
+
+function formated_exec(command) {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(chalk.red(`exec error: ${error}`));
+          console.error(chalk.red(`stderr: ${stderr}`));
+          return;
+        }
+        console.log(chalk.green(`stdout: ${stdout}`));        
+      });
+}
+
+function new_solution(name) {
+    formated_exec_sync(`dotnet new sln --name ${name}`);
+}
+
+function new_console_project(name) {
+    formated_exec_sync(`dotnet new console -lang "C#" --name ${name}`);
+}
+
+function add_project_to_solution(project_name, solution_name) {
+    formated_exec_sync(`dotnet sln ${solution_name}.sln add ${project_name}`);
+}
+
+function add_package_to_project(package, project, version = null) {
+    let command = `dotnet add ${project} package`;
+    if (version != null)
+        command += ` --version ${version}`;
+    command += ` ${package}`
+    formated_exec_sync(command);
+}
+
+module.exports = {
+    new_solution,
+    new_console_project,
+    add_project_to_solution,
+    add_package_to_project,
+}
--- app/index.js
+++ app/index.js
@@ -1,11 +1,44 @@
-var Generator = require('yeoman-generator');
-var _ = require('lodash');
-var yosay = require('yosay');
-var chalk = require('chalk');
-var pkg = require('../package.json');
+const Generator = require('yeoman-generator');
+const _ = require('lodash');
+const yosay = require('yosay');
+const chalk = require('chalk');
+const pkg = require('../package.json');
+const dotnet = require('./dotnet');
+
+function create_solution_and_projects(answers) {
+  /* */
+  dotnet.new_solution(answers.name);
+  
+  dotnet.new_console_project("Core");
+  dotnet.new_console_project("Database");
+  
+  dotnet.add_project_to_solution("Core", answers.name);
+  dotnet.add_project_to_solution("Database", answers.name);
+
+  dotnet.add_package_to_project("dbup", "Database", "4.2.0");
+  
+  dotnet.add_package_to_project("Microsoft.Extensions.Logging", "Core", "2.2.0");
+  dotnet.add_package_to_project("Microsoft.Extensions.Logging.Console", "Core", "2.2.0");
+  dotnet.add_package_to_project("Microsoft.Extensions.Hosting", "Core", "2.2.0");
+  dotnet.add_package_to_project("Microsoft.Extensions.DependencyInjection", "Core", "2.2.0");
+  dotnet.add_package_to_project("Microsoft.Extensions.Configuration.Json", "Core", "2.2.0");
+  dotnet.add_package_to_project("Microsoft.Extensions.Configuration", "Core", "2.2.0");
+}
 
 module.exports = class extends Generator {
-    prompting() {        
-		this.log(yosay('Welcome to the Yeoman ' + chalk.green('micron') + ' (' + pkg.version + ')' + ' generator!'));
+    async prompting() {
+      this.log(yosay('Welcome to the Yeoman ' + chalk.green('micron') + ' (' + pkg.version + ')' + ' generator!'));
+      
+      this.answers = await this.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Microservice name"
+        }
+      ]);
+    }
+
+    writing() {
+      create_solution_and_projects(this.answers);
     }
 }
