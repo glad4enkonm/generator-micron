0b54592d0c419d4f027f101de760cb478cfd5625
glad4enkonm <glad4enkonm@gmail.com>
2024-11-05 00:01:29 +0300

    migrations topological sort +

--- migrations/index.js
+++ migrations/index.js
@@ -3,6 +3,29 @@ const sql = require("../common/sql")
 const fs = require('fs')
 const {pad} = require("../common/naming");
 const {appendHistoryEntities} = require("../common/history");
+const { result } = require("lodash");
+const TopologicalSort = require("../utill/topological")
+
+
+function myTopologicalSort(arr) {    
+    nodes = new Map()
+    for (const element of arr)
+        nodes.set(element["name"], element)    
+    const sortOp = new TopologicalSort(nodes)    
+    
+    for (const element of arr) {
+        for (const relation of element["relations"]) {
+            sortOp.addEdge(element["name"], relation["table"])
+            console.log(element["name"], "->", relation["table"])
+        }
+    }
+    
+    const sorted = sortOp.sort()    
+    const sortedValues = [...sorted.keys()].reverse().map(e => nodes.get(e))    
+    return sortedValues
+
+  }
+
 
 function copyDbFiles(that) {
     const migrationsSubdir = that.options.calledFromApp ? 'database/Migrations/' : 'Migrations/'
@@ -11,7 +34,9 @@ function copyDbFiles(that) {
     const newMigrationNumber = files.length > 0 ? Number(files[0].split('_')[0]) + 1 : 0
     appendHistoryEntities(that)
 
-    that.answers.entities.map(entity => {
+    sortedEntities = myTopologicalSort(that.answers.entities, e => e.relations.length)
+
+    sortedEntities.map(entity => {
         if (entity.hasOwnProperty("generation") && entity.generation.migrations == false)
             return // пропускаем если у элемента есть указание не создавать
 
@@ -20,7 +45,7 @@ function copyDbFiles(that) {
         that.fs.copyTpl(
             that.templatePath("Migrations/_000N_DESC.sql"),
             that.destinationPath(migrationsSubdir +
-                `${pad(newMigrationNumber + that.answers.entities.indexOf(entity),4)}_${dataToRender.name}.sql`),
+                `${pad(newMigrationNumber + sortedEntities.indexOf(entity),4)}_${dataToRender.name}.sql`),
             dataToRender
         )
     })
